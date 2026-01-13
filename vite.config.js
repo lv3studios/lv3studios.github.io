ed7064f70bf18841c3849814dc23b4ac85ebda25
frostyral <csjjgarcia@gmail.com>
2026-01-14 02:36:48 +0800

    chore: update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  base: '/', // This is the default, but it's good to be explicit
+  base: '/lv3studios.github.io/', // This is the default, but it's good to be explicit
   plugins: [
     tailwindcss(),
   ],
